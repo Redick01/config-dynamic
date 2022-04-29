f7ca3b4ecff6fa3531d6145355835a68534842a6
Redick01 <11090829@qq.com>
2022-04-29 14:33:23 +0800

    add log4j MDCAdapter

--- datachange/server/src/main/java/com/redick/datachange/server/Server.java
+++ datachange/server/src/main/java/com/redick/datachange/server/Server.java
@@ -1,16 +1,45 @@
 package com.redick.datachange.server;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.slf4j.TtlMDCAdapter;
+import org.slf4j.spi.MDCAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.UUID;
 
 /**
  * @author liupenghui
  * @date 2022/2/14 2:08 下午
  */
 @SpringBootApplication
+@Slf4j
+@RestController
 public class Server {
 
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        MDCAdapter mdcAdapter = TtlMDCAdapter.getInstance();
+        log.info("MDCAdapter实例：{}", mdcAdapter);
+    }
+
+    @GetMapping("/MDCAdapterTest")
+    public String getTest() {
+        MDC.put("traceId", UUID.randomUUID().toString());
+        log.info("开始测试");
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                log.info("当前线程：{}", Thread.currentThread().getName().toString());
+            }).start();
+        }
+        return "success";
     }
 }
---
+++ datachange/server/src/main/java/org/slf4j/TtlMDCAdapter.java
@@ -0,0 +1,161 @@
+package org.slf4j;
+
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+import org.slf4j.spi.MDCAdapter;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 重写logback的LogbackMDCAdapter，注意MDC是MDCAdapter是包级私有，所以重写的报名应该是org.slf4j
+ * 用TransmittableThreadLocal替换ThreadLocal，解决多线程，异步情况traceId无法传递问题
+ *
+ * @author Redick01
+ */
+@SuppressWarnings("all")
+public class TtlMDCAdapter implements MDCAdapter {
+
+    private final ThreadLocal<Map<String, String>> copyOnInheritThreadLocal = new TransmittableThreadLocal<>();
+
+    private static final int WRITE_OPERATION = 1;
+    private static final int MAP_COPY_OPERATION = 2;
+
+    static TtlMDCAdapter mtcMDCAdapter;
+
+    static {
+        mtcMDCAdapter = new TtlMDCAdapter();
+        MDC.mdcAdapter = mtcMDCAdapter;
+    }
+
+    public static MDCAdapter getInstance() {
+        return mtcMDCAdapter;
+    }
+
+    final ThreadLocal<Integer> lastOperation = new ThreadLocal<Integer>();
+
+    private Integer getAndSetLastOperation(int op) {
+        Integer lastOp = lastOperation.get();
+        lastOperation.set(op);
+        return lastOp;
+    }
+
+    private boolean wasLastOpReadOrNull(Integer lastOp) {
+        return lastOp == null || lastOp.intValue() == MAP_COPY_OPERATION;
+    }
+
+    private Map<String, String> duplicateAndInsertNewMap(Map<String, String> oldMap) {
+        Map<String, String> newMap = Collections.synchronizedMap(new HashMap<String, String>());
+        if (oldMap != null) {
+            // we don't want the parent thread modifying oldMap while we are
+            // iterating over it
+            synchronized (oldMap) {
+                newMap.putAll(oldMap);
+            }
+        }
+
+        copyOnInheritThreadLocal.set(newMap);
+        return newMap;
+    }
+
+    @Override
+    public void put(String key, String val) throws IllegalArgumentException {
+        if (key == null) {
+            throw new IllegalArgumentException("key cannot be null");
+        }
+
+        Map<String, String> oldMap = copyOnInheritThreadLocal.get();
+        Integer lastOp = getAndSetLastOperation(WRITE_OPERATION);
+
+        if (wasLastOpReadOrNull(lastOp) || oldMap == null) {
+            Map<String, String> newMap = duplicateAndInsertNewMap(oldMap);
+            newMap.put(key, val);
+        } else {
+            oldMap.put(key, val);
+        }
+    }
+
+    @Override
+    public void remove(String key) {
+        if (key == null) {
+            return;
+        }
+        Map<String, String> oldMap = copyOnInheritThreadLocal.get();
+        if (oldMap == null)
+            return;
+
+        Integer lastOp = getAndSetLastOperation(WRITE_OPERATION);
+
+        if (wasLastOpReadOrNull(lastOp)) {
+            Map<String, String> newMap = duplicateAndInsertNewMap(oldMap);
+            newMap.remove(key);
+        } else {
+            oldMap.remove(key);
+        }
+    }
+
+    @Override
+    public void clear() {
+        lastOperation.set(WRITE_OPERATION);
+        copyOnInheritThreadLocal.remove();
+    }
+
+    @Override
+    public String get(String key) {
+        final Map<String, String> map = copyOnInheritThreadLocal.get();
+        if ((map != null) && (key != null)) {
+            return map.get(key);
+        } else {
+            return null;
+        }
+    }
+
+    public Map<String, String> getPropertyMap() {
+        lastOperation.set(MAP_COPY_OPERATION);
+        return copyOnInheritThreadLocal.get();
+    }
+
+    public Set<String> getKeys() {
+        Map<String, String> map = getPropertyMap();
+
+        if (map != null) {
+            return map.keySet();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Map<String, String> getCopyOfContextMap() {
+        Map<String, String> hashMap = copyOnInheritThreadLocal.get();
+        if (hashMap == null) {
+            return null;
+        } else {
+            return new HashMap<String, String>(hashMap);
+        }
+    }
+
+    @Override
+    public void setContextMap(Map contextMap) {
+        lastOperation.set(WRITE_OPERATION);
+
+        Map<String, String> newMap = Collections.synchronizedMap(new HashMap<String, String>());
+        newMap.putAll(contextMap);
+
+        // the newMap replaces the old one for serialisation's sake
+        copyOnInheritThreadLocal.set(newMap);
+    }
+
+//    @Override
+//    public void setContextMap(Map<String, String> contextMap) {
+//        lastOperation.set(WRITE_OPERATION);
+//
+//        Map<String, String> newMap = Collections.synchronizedMap(new HashMap<String, String>());
+//        newMap.putAll(contextMap);
+//
+//        // the newMap replaces the old one for serialisation's sake
+//        copyOnInheritThreadLocal.set(newMap);
+//    }
+}
